dddaf5160aa434b994fd9d7250210efe17729526
sara toftegaard <satoftegaard@gmail.com>
2016-11-21 18:50:07 -0500

    color-picker done

--- public/main.js
+++ public/main.js
@@ -1,6 +1,41 @@
-const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+let hue = 360
+let saturation = 100
+let light = 50
+let alpha = 1
+
+// hsla(360, 100%, 50%, 1)
+const getHSL = () => {
+  return `hsla(${hue}, ${saturation}%, ${light}%, ${alpha})`
 }
+const main = () => {
+  const swatch = document.querySelector('.box')
 
+  const hueInput = document.querySelector('input[name=hue]')
+  hueInput.addEventListener('input', () => {
+    hue = hueInput.value
+    swatch.style.backgroundColor = getHSL()
+    document.querySelector('p').textContent = getHSL()
+  })
+
+  const satInput = document.querySelector('input[name=saturation]')
+  satInput.addEventListener('input', () => {
+    saturation = satInput.value
+    swatch.style.backgroundColor = getHSL()
+    document.querySelector('p').textContent = getHSL()
+  })
+
+  const lightInput = document.querySelector('input[name=light]')
+  lightInput.addEventListener('input', () => {
+    light = lightInput.value
+    swatch.style.backgroundColor = getHSL()
+    document.querySelector('p').textContent = getHSL()
+  })
+
+  const alphaInput = document.querySelector('input[name=alpha]')
+  alphaInput.addEventListener('input', () => {
+    alpha = alphaInput.value
+    swatch.style.backgroundColor = getHSL()
+    document.querySelector('p').textContent = getHSL()
+  })
+}
 document.addEventListener('DOMContentLoaded', main)
-
